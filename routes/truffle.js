913d3b574f6aa60c2be4ef0f22a94f71ac864ba7
Jonne Pihlanen <jonne.pihlanen@gmail.com>
2017-06-19 20:44:38 +0300

    korjaus promisien käyttöön

--- routes/truffle.js
+++ routes/truffle.js
@@ -1,30 +1,20 @@
-var express = require('express');
-var router = express.Router();
-var Web3 = require("web3");
-var contract = require("truffle-contract");
-var employmentcontract_artifacts = require("../truffle/build/contracts/EmploymentContract.json")
+const express = require('express');
+const router = express.Router();
+const Web3 = require('web3');
+const contract = require('truffle-contract');
+const employmentcontract_artifacts = require('../truffle/build/contracts/EmploymentContract.json');
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-
-    var provider = new Web3.providers.HttpProvider("http://localhost:8545");
-    var EmploymentContract = contract(employmentcontract_artifacts);
+router.get('/', (req, res, next) => {
+    const provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    const EmploymentContract = contract(employmentcontract_artifacts);
     EmploymentContract.setProvider(provider);
-    
-    // arvo tulee alla promisen kautta, mutta se ei kerkee
-    // res.renderiin mukaan?
-    var deployedAddress; 
 
-    EmploymentContract.deployed().then(function (instance) {
-        console.log(instance.address) // lokittuu res.renderin jälkeen
-        deployedAddress = instance.address;
-    });
-
-    res.render('truffle', {
-        title: 'EmploymentContract testing',
-        deployedAddress: deployedAddress
-    });
-
+    return EmploymentContract.deployed()
+        .then((instance) => res.render('truffle', {
+            title: 'EmploymentContract testing',
+            deployedAddress: deployedAddress
+        }));
 });
 
 module.exports = router;
