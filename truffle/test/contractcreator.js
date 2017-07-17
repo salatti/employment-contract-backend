b2d369e012c4f1f5cae48396dead9d0830216467
Altti Hiironen <altti.hiironen@student.tut.fi>
2017-07-17 09:55:28 +0300

    Update test

--- truffle/test/contractcreator.js
+++ truffle/test/contractcreator.js
@@ -1,5 +1,12 @@
 /* global artifacts:true contract:true assert:true */
+const Web3 = require('web3');
+const moment = require('moment');
+
+const web3 = new Web3();
 const ContractCreator = artifacts.require('./ContractCreatorTestable.sol');
+const employeeName = web3.fromAscii('Test User');
+let employeeAddr;
+const lastAccTime = moment(new Date()).unix();
 
 contract('ContractCreator', (accounts) => {
   it('should set owner correctly', () => ContractCreator.deployed()
@@ -13,4 +20,22 @@ contract('ContractCreator', (accounts) => {
     .then((result) => {
       assert.equal(result, 0, 'there was created contracts');
     }));
+
+  it('should not allow contract creation from other than owner', () => {
+    let ContractCreatorInstance;
+
+    return ContractCreator.deployed()
+      .then((instance) => {
+        ContractCreatorInstance = instance;
+        return ContractCreatorInstance.createEmplyoymentContract(accounts[2],
+          employeeName, web3.toBigNumber(lastAccTime), { from: accounts[1] });
+      })
+      .then(assert.fail)
+      .catch((error) => {
+        assert(
+          error.message.indexOf('invalid opcode') >= 0,
+          'should throw invalid opcode exception.'
+        );
+      });
+  });
 });
